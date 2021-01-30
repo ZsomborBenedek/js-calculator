2b4a28a65db856aee8f0283383e14ca5ff4fe16e
Zsombor Benedek <Zsombor Benedek@DESKTOP-VIMNCSB>
2021-01-30 23:38:46 +0100

    created simplest possible API

--- js-calculator-backend/index.js
+++ js-calculator-backend/index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express(),
-   json = express.json,
+   json = express.json(),
    port = parseInt(process.env.PORT, 10) || 8080;
-
+const fs = require('fs');
 
 // Get the memory
 app.get('/mem', (req, res) => {
-   console.log('This is a GET request');
+   fs.readFile(__dirname + '/db/mem.json', (err, data) => {
+      if (err) return console.error(err);
+      data = JSON.parse(data);
+      console.log(data);
+      res.json(data);
+   });
 });
 
 // Set the memory
 app.post('/mem', json, (req, res) => {
-   console.log('This is a POST request');
+   fs.readFile(__dirname + '/db/mem.json', (err, data) => {
+      if (err) return console.error(err);
+      data = JSON.parse(data);
+      data.id = req.body.id;
+      data.value = req.body.value;
+      fs.writeFile(__dirname + '/db/mem.json', JSON.stringify(data), (err) => {
+         if (err) return console.error(err);
+         res.sendStatus(200);
+      });
+   });
 });
 
 app.listen(port, () => {
