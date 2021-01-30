36c0ed0f2a5ac2511dbe6983e1e752bca1cbcfe3
Zsombor Benedek <Zsombor Benedek@DESKTOP-VIMNCSB>
2021-01-30 19:57:34 +0100

    created index.js

---
+++ js-calculator-backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = parseInt(process.env.PORT, 10) || 8080;
+
+
+// Get the memory
+app.get('/mem', (req, res) => {
+   console.log('This is a GET request');
+});
+
+// Set the memory
+app.post('/mem', json, (req, res) => {
+   console.log('This is a POST request');
+});
+
+app.listen(port, () => {
+   console.log(`Calculator backend listening at http://localhost:${port}`);
+});
