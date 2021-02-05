502d5430fb5c9dc1c6d79c5f23ef50adfcd9f3d9
Zsombor Benedek <Zsombor Benedek@DESKTOP-VIMNCSB>
2021-02-05 04:00:54 +0100

    cors module added

--- js-calculator-backend/index.js
+++ js-calculator-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 const FileStore = require('session-file-store')(session);
 const app = express(),
    json = express.json(),
@@ -15,13 +16,22 @@ app.use(session({
    cookie: { maxAge: 3600000, secure: false, httpOnly: true }
 }));
 
+app.use(cors({
+   origin: ['http://localhost:3000'],
+   methods: ['GET', 'POST'],
+   credentials: true
+}));
+
 // Get the memory
 app.get('/api/mem', (req, res) => {
+   console.log('GET - ', req.sessionID);
    try {
       sessionStore.get(req.sessionID, function (err, session) {
          if (session) {
+            console.log('session found')
             return err ? res.sendStatus(500) : res.json({ "memory": session.memory });
          } else {
+            console.log('session not found')
             return res.sendStatus(404);
          }
       });
@@ -33,6 +43,7 @@ app.get('/api/mem', (req, res) => {
 
 // Set the memory
 app.post('/api/mem', json, (req, res) => {
+   console.log('POST - ', req.sessionID);
    try {
       req.session.memory = parseInt(req.body.memory);
       sessionStore.set(req.sessionID, req.session, function (err) {
